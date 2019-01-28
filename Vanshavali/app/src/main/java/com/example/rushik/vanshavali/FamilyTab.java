0573cb90f9aeb412bd62ea7bc5b5b8a67675586e
rushik maniar <rushikmaniar107@gmail.com>
2019-01-28 14:20:35 +0530

    some changes done

--- Vanshavali/app/src/main/java/com/example/rushik/vanshavali/FamilyTab.java
+++ Vanshavali/app/src/main/java/com/example/rushik/vanshavali/FamilyTab.java
@@ -56,14 +56,6 @@ public class FamilyTab extends AppCompatActivity {
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
     }
 
@@ -145,7 +137,7 @@ public class FamilyTab extends AppCompatActivity {
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 4;
         }
     }
 }
